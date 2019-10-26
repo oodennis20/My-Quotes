44691bdb48765c991ec98c5b993cf476792ca268
oodennis20 <oodennis20@gmail.com>
2019-10-26 22:13:31 +0300

    add a class Quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'My-quotes';
-  quote:string[];
-
-  constructor(){
-    this.quotes = ['If you are always trying to be normal you will never know how amazing you can be', 'Procastination is the thief of time', 'There is no secret way to succes. It is the result of preparation, hardwork, and learning from failure']
-  }
+  quotes:Quote= [
+    {class:1, name:'If you are always trying to be normal you will never know how amazing you can be'},
+    {class:2, name:'Procastination is the thief of time'},
+    {class:3, name:'There is no secret way to succes. It is the result of preparation, hardwork, and learning from failure'},
+  ];
 }
---
+++ src/app/quote.ts
@@ -0,0 +1,4 @@
+export class Quote {
+    class: number;
+    name: string;
+}
