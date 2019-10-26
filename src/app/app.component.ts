c7c83ed073301c558f2a8bc912bb4b45400bfe5a
oodennis20 <oodennis20@gmail.com>
2019-10-26 20:55:32 +0300

    add some quotes in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My-quotes';
+  quote:string[];
+
+  constructor(){
+    this.quotes = ['If you are always trying to be normal you will never know how amazing you can be', 'Procastination is the thief of time', 'There is no secret way to succes. It is the result of preparation, hardwork, and learning from failure']
+  }
 }
